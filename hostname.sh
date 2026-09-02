9d2413e7b43fe2250ca8a37de7981f0eec17bea4
YASVANTH KANNAN L <yasvanthkannan55@gmail.com>
2026-09-02 10:25:22 +0530

    UNIX PROGRAM 2 IU25IT118
    Removed echo statements to streamline the script.

--- hostname.sh
+++ hostname.sh
@@ -1,10 +1,7 @@
+
 #!/bin/bash
 
-echo "Current hostname:"
 hostnamectl
-
-echo "Changing hostname to rvs..."
 hostnamectl set-hostname rvs
-
-echo "Starting new Bash shell..."
 exec bash
+hostnamectl
